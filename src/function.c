d8ef0f88ff8a931b50bf5704825e7883f5d0fa47
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-01-19 10:38:00 +0100

    Implement target function and its gradient

---
+++ src/function.c
@@ -0,0 +1,12 @@
+#include "function.h"
+
+double function_F(const Point p) {
+    return 100 * (p.x - (p.x * p.x)) * (p.y - (p.x * p.x)) + (1 - p.x) * (1 - p.x);
+}
+
+Vector gradient_F(const Point p) {
+    return (Vector) {
+        .x = 2 * ((200 * p.x * p.x * p.x) - (200 * p.x * p.y) + p.x - 1),
+        .y = 200 * (p.y - p.x * p.x),
+    };
+}
