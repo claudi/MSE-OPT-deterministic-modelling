4e0a9d5fef9a718a2dd5ff99733dca9f5f83300f
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-02-06 18:25:37 +0100

    Fix `function_F()` formula

--- src/function.c
+++ src/function.c
@@ -1,7 +1,7 @@
 #include "function.h"
 
 double function_F(const Point p) {
-    return 100 * (p.x - (p.x * p.x)) * (p.y - (p.x * p.x)) + (1 - p.x) * (1 - p.x);
+    return 100 * (p.y - (p.x * p.x)) * (p.y - (p.x * p.x)) + (1 - p.x) * (1 - p.x);
 }
 
 Vector gradient_F(const Point p) {
